a7016207249a060d7ad035417ace5c9cd0e7f40a
OrangeBird <yuanxinbj@gmail.com>
2024-03-26 11:54:08 +0800

    main

--- main.py
+++ main.py
@@ -6,12 +6,29 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
 }
 
-r = requests.get(url=url,headers=headers)
-soup = BeautifulSoup(r.content,"html.parser")
-anchor = soup.find_all("a")
+r = requests.get(url=url, headers=headers)
+soup = BeautifulSoup(r.content, "html.parser")
+anchors = soup.find_all("a")
 images = soup.find_all("img")
-for a in anchor:
-    src = a.img['src']
-    newUrl = "http://motions.cat/{}".format(images)
-    # print(newUrl)
-    print(src)
+# for image in images[3:-5]:
+#     src = image["src"]
+#     imageUrl = "http://motions.cat/gif/nhn/0146.gif"
+for anchor in anchors[4:-6]:
+    try:
+        href = anchor["href"]
+        imageUrl = "http://motions.cat/{}".format(href)
+        # part = href.split(".gif")[0]
+        # filename = part.split("/")[-1]
+        gifFile = href.split("/")[-1]
+        filename = gifFile.split(".gif")[0]
+        getRequest = requests.get(imageUrl, headers=headers)
+        if getRequest.status_code == 200:
+            with open(gifFile, "wb") as f:
+                # write with binary for "wb"
+                # read with binary for "rb"
+                fileWrite = f.write(getRequest.content)
+                print("Saving {} now...".format(gifFile))
+        else:
+            print("Error getting {}.".format(gifFile))
+    except KeyError:
+        print(KeyError)
